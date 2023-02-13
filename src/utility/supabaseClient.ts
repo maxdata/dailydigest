9ba44f159fffaa3d623345cab8021f8623edb12f
MaxData <max@workhere.org>
2023-02-13 14:08:19 -0500

    update supabase

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -4,8 +4,8 @@ import { createClient } from "@pankod/refine-supabase";
 // const SUPABASE_KEY =
 //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const SUPABASE_URL = process.env.SUPABASE_URL as string;
+const SUPABASE_KEY = process.env.SUPABASE_ANON_KEY as string;
   
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
