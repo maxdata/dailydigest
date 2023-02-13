350fea0ea43252d462c78041bc68360a90aba687
MaxData <max@workhere.org>
2023-02-13 15:00:09 -0500

    REACT_APP_SUPABASE_URL

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -4,9 +4,10 @@ import { createClient } from "@pankod/refine-supabase";
 // const SUPABASE_KEY =
 //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";
 
-const SUPABASE_URL = process.env.SUPABASE_URL as string;
-const SUPABASE_KEY = process.env.SUPABASE_ANON_KEY as string;
-  
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL as string;
+const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY as string;
+
+
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   db: {
