35e2be392f9a4d9e14c115bf131c040a03b9fbd6
MaxData <max@workhere.org>
2023-02-17 16:53:30 -0500

    add github

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,150 @@
-import { Refine } from "@pankod/refine-core";
+import { AuthProvider, Refine } from "@pankod/refine-core";
 import {
-  AuthPage,
-  notificationProvider,
-  ChakraProvider,
-  refineTheme,
-  ReadyPage,
-  ErrorComponent,
+    AuthPage,
+    ChakraProvider,
+    Layout,
+    ErrorComponent,
+    ReadyPage,
+    refineTheme,
+    notificationProvider,
 } from "@pankod/refine-chakra-ui";
-
-import { dataProvider, liveProvider } from "@pankod/refine-supabase";
+import { dataProvider} from "@pankod/refine-supabase";
 import routerProvider from "@pankod/refine-react-router-v6";
-import { supabaseClient } from "utility";
-import { Title, Sider, Layout, Header } from "components/layout";
-import authProvider from "./authProvider";
-import { PostCreate, PostEdit, PostList, PostShow } from "pages";
+import { IconBrandGoogle, IconBrandGithub } from "@tabler/icons";
+import { PostCreate, PostEdit, PostList, PostShow } from "./pages";
+import { supabaseClient } from "utility/supabaseClient";
 
-function App() {
-  return (
-    <ChakraProvider theme={refineTheme}>
-      <Refine
-        dataProvider={dataProvider(supabaseClient)}
-        liveProvider={liveProvider(supabaseClient)}
-        authProvider={authProvider}
-        routerProvider={{
-          ...routerProvider,
-          routes: [
-            {
-              path: "/register",
-              element: <AuthPage type="register" />,
-            },
-            {
-              path: "/forgot-password",
-              element: <AuthPage type="forgotPassword" />,
-            },
-            {
-              path: "/update-password",
-              element: <AuthPage type="updatePassword" />,
-            },
-          ],
-        }}
-        LoginPage={() => (
-          <AuthPage
-            type="login"
-            providers={[
-              {
-                name: "google",
-                label: "Sign in with Google",
-              },
-            ]}
-            formProps={{
-              // defaultValues: {
-              //   email: "info@refine.dev",
-              //   password: "refine-supabase",
-              // },
-            }}
-          />
-        )}
-        notificationProvider={notificationProvider()}
-        ReadyPage={ReadyPage}
-        catchAll={<ErrorComponent />}
-        resources={[
-          {
-            name: "posts",
-            list: PostList,
-            create: PostCreate,
-            edit: PostEdit,
-            show: PostShow,            
-            // canDelete: true,
-          },
-        ]}
-        Title={Title}
-        Sider={Sider}
-        Layout={Layout}
-        Header={Header}
-      />
-    </ChakraProvider>
-  );
-}
+const App: React.FC = () => {
+    const authProvider: AuthProvider = {
+        login: async ({ providerName, email }) => {
+            if (providerName === "google") {
+                window.location.href =
+                    "https://accounts.google.com/o/oauth2/v2/auth";
+                return Promise.resolve(false);
+            }
+
+            if (providerName === "github") {
+                window.location.href =
+                    "https://github.com/login/oauth/authorize";
+                return Promise.resolve(false);
+            }
+
+            if (email) {
+                localStorage.setItem("email", email);
+                return Promise.resolve();
+            }
+
+            return Promise.reject();
+        },
+        register: (params) => {
+            if (params.email && params.password) {
+                localStorage.setItem("email", params.email);
+                return Promise.resolve();
+            }
+            return Promise.reject();
+        },
+        updatePassword: (params) => {
+            if (params.newPassword) {
+                //we can update password here
+                return Promise.resolve();
+            }
+            return Promise.reject();
+        },
+        forgotPassword: (params) => {
+            if (params.email) {
+                //we can send email with reset password link here
+                return Promise.resolve();
+            }
+            return Promise.reject();
+        },
+        logout: () => {
+            localStorage.removeItem("email");
+            return Promise.resolve();
+        },
+        checkError: () => Promise.resolve(),
+        checkAuth: () =>
+            localStorage.getItem("email")
+                ? Promise.resolve()
+                : Promise.reject(),
+        getPermissions: () => Promise.resolve(["admin"]),
+        getUserIdentity: () =>
+            Promise.resolve({
+                id: 1,
+                name: "Jane Doe",
+                avatar: "https://unsplash.com/photos/IWLOvomUmWU/download?force=true&w=640",
+            }),
+    };
+
+    return (
+        <ChakraProvider theme={refineTheme}>
+            <Refine
+                dataProvider={dataProvider(supabaseClient)}
+                authProvider={authProvider}
+                notificationProvider={notificationProvider()}
+                routerProvider={{
+                    ...routerProvider,
+                    routes: [
+                        {
+                            path: "/register",
+                            element: (
+                                <AuthPage
+                                    type="register"
+                                    providers={[
+                                        {
+                                            name: "google",
+                                            label: "Sign in with Google",
+                                            icon: <IconBrandGoogle />,
+                                        },
+                                        {
+                                            name: "github",
+                                            label: "Sign in with GitHub",
+                                            icon: <IconBrandGithub />,
+                                        },
+                                    ]}
+                                />
+                            ),
+                        },
+                        {
+                            path: "/forgot-password",
+                            element: <AuthPage type="forgotPassword" />,
+                        },
+                        {
+                            path: "/update-password",
+                            element: <AuthPage type="updatePassword" />,
+                        },
+                    ],
+                }}
+                LoginPage={() => (
+                    <AuthPage
+                        providers={[
+                            {
+                                name: "google",
+                                label: "Sign in with Google",
+                                icon: <IconBrandGoogle />,
+                            },
+                            {
+                                name: "github",
+                                label: "Sign in with GitHub",
+                                icon: <IconBrandGithub />,
+                            },
+                        ]}
+                    />
+                )}
+                ReadyPage={ReadyPage}
+                catchAll={<ErrorComponent />}
+                Layout={Layout}
+                resources={[
+                    {
+                        name: "posts",
+                        list: PostList,
+                        show: PostShow,
+                        edit: PostEdit,
+                        create: PostCreate,
+                    },
+                ]}
+            />
+        </ChakraProvider>
+    );
+};
 
 export default App;
